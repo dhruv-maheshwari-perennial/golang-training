4f8e73673ec6eb68eabdf96f1f96cba03aea0b7b
Dhruv Maheshwari <dhruv.maheshwari@perennialsys.com>
2022-08-29 11:25:49 +0530

    Completed constants-iota & conditions-loop

--- 03-constants/01-constants.go
+++ 03-constants/01-constants.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt"
 
+const e = 123        // outer scope
+const name = "Dhruv" // global const variable
+
+// go doesn't support const map, struct, array & slice
 func main() {
+	// local const variables
 	const a string = "10"
 	const b = 10
 	const (
@@ -10,8 +15,16 @@ func main() {
 		d = 2.43
 	)
 
+	const e = 456 // inner scope
+	testGlobal()
+
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
 	fmt.Println(d)
+	fmt.Println(e) // output - 456 of inner scope constant
+}
+
+func testGlobal() {
+	fmt.Println(name)
 }
